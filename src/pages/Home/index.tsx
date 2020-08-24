b34197724041f45169732652c266f3faf1dc2a4e
Roberto Maia <59492898+romaiajr@users.noreply.github.com>
2020-08-24 11:12:20 -0300

    Buttons Section

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/NavBar'
 import { Link } from 'react-router-dom';
+import LogoNome from '../../assets/images/logo_nome2.svg';
+import MainImage from '../../assets/images/hero_image.svg';
 import './styles.css';
-import HeroImage from '../../components/HeroImage';
 
 function Home() {
     return (
-        <div>
+        <div className="main_page">
             <Navbar>
                 <Link to="" className="nav_link">Atrações</Link>
                 <Link to="" className="nav_link">Informações</Link>
@@ -14,8 +15,26 @@ function Home() {
                 <Link to="" className="nav_link">Agendamento</Link>
                 <Link to="" className="nav_link">Login</Link>
             </Navbar>
-            <HeroImage />
-        </div>
+            <div className="brand_section">
+                <img src={MainImage} alt="Imagem Principal" className="main_img" />
+                <div className="container">
+                    <img src={LogoNome} alt="Logo Nome" className="logo_nome" />
+                    <p>Ficamos muito felizes em <br />receber a <b>sua visita ;)</b></p>
+                </div>
+            </div>
+            <div className="scheduling_section">
+                <div className="container_grid">
+
+                    <p>Seja bem-vindo!<br /><b>Agende já a sua visita</b></p>
+
+                    <span>Realizar agendamento como:</span>
+                    <div className="button_container">
+                        <Link to="/" id="pessoa_btn" className="link_button">Pessoa</Link>
+                        <Link to="/" id="escola_btn">Escola</Link>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 }
 
