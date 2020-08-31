7145d6ddb86649ccf7edbef8d0d5839ed4762844
Roberto Maia <59492898+romaiajr@users.noreply.github.com>
2020-08-31 20:11:05 -0300

    Arrumando Navbar

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap';
 import Logo from '../../assets/images/logo.svg'
+import { Link } from 'react-router-dom';
 import './style.css';
 
 interface navProps {
@@ -11,9 +12,9 @@ const NavBar: React.FunctionComponent<navProps> = (props) => {
     return (
         <nav className="navbar">
             <div className="logo_title">
-                <a href="/">
+                <Link to="/">
                     <img className="logo" src={Logo} alt="Logo Antares" />
-                </a>
+                </Link>
                 <h3>Observatório Astronômico Antares</h3>
             </div>
             <div>
